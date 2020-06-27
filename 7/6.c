ff565fa3e7b53329a19064dea33f97547cbea1bf
root <2014164192@qq.com>
2020-06-27 20:17:37 +0800

    review and rewrite

--- 7/6.c
+++ 7/6.c
@@ -4,7 +4,7 @@ int main ()
 {	
     int year, month, day;
 
-    scanf("%d%-%d-%d", &year, &month, &day);    
+    scanf("%d-%d-%d", &year, &month, &day);    
  
     if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && (day >= 1 && day <=31)) {
         printf("YES");
