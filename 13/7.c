b534131991be1139362736dd36e251b7b45e4924
HazeFlood <67228421+HazeFlood@users.noreply.github.com>
2020-06-27 22:04:35 +0800

    Update 7.c

--- 13/7.c
+++ 13/7.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 typedef struct node {
     int data;
     struct node *next;
@@ -8,7 +9,8 @@ typedef struct node {
 Node *circle_create(int n);
 void count_off(Node *head, int n, int k, int m);
 
-int main() {
+int main() 
+{
     int n, k, m;
     scanf("%d%d%d", &n, &k, &m);
     Node *head = circle_create(n);
@@ -16,16 +18,17 @@ int main() {
     return 0;
 }
 
-Node *circle_create(int n) {
+Node *circle_create(int n) 
+{
     Node *temp, *new_node, *head;
     int i;
 
-    // ´´½¨µÚÒ»¸öÁ´±í½Úµã²¢¼ÓÊı¾İ
+    // åˆ›å»ºç¬¬ä¸€ä¸ªé“¾è¡¨èŠ‚ç‚¹å¹¶åŠ æ•°æ®
     temp = (Node *) malloc(sizeof(Node));
     head = temp;
     head->data = 1;
 
-    // ´´½¨µÚ 2 µ½µÚ n ¸öÁ´±í½Úµã²¢¼ÓÊı¾İ
+    // åˆ›å»ºç¬¬ 2 åˆ°ç¬¬ n ä¸ªé“¾è¡¨èŠ‚ç‚¹å¹¶åŠ æ•°æ®
     for(i = 2; i <= n; i++) {
         new_node = (Node *) malloc(sizeof(Node));
         new_node->data = i;
@@ -33,33 +36,49 @@ Node *circle_create(int n) {
         temp = new_node;
     }
 
-    // ×îºóÒ»¸ö½ÚµãÖ¸ÏòÍ·²¿¹¹³ÉÑ­»·Á´±í
+    // æœ€åä¸€ä¸ªèŠ‚ç‚¹æŒ‡å‘å¤´éƒ¨æ„æˆå¾ªç¯é“¾è¡¨
     temp->next = head;
 
     return head;
 }
 
-void count_off(Node *head, int n, int k, int m) {
-    Node *p=head;
+void count_off(Node *head, int n, int k, int m) 
+{
+    Node *p = head;
     Node *q;
-    if(p!=NULL){
-    	while(p->data!=k){
-        	p=p->next;
+    int i;
+    if(p != NULL && m != 1){
+    	while(p->data != k){
+        	p = p->next;
         }
-        while(p!=p->next){
+        while(p != p->next){
             int j=1;
-            while(j<m-1){
-            	p=p->next;
+            while(j < m-1){
+            	p = p->next;
             	++j;
 			}
-            q=p->next;
+            q = p->next;
             printf("%d ",q->data);
-            p->next=q->next;
+            p->next = q->next;
             free(q);
-            q=NULL;
-            p=p->next;
+            q = NULL;
+            p = p->next;
         }
-        printf("%d",p->data);
+        printf("%d", p->data);
+    }else if (m == 1 && k == 1){
+    	for (i = 1; i <= n; i++){
+    		printf("%d", i);
+    		if( i != n){
+    			printf(" ");
+			}
+		}
+	}else if (m == 1 && k != 1){
+	 for (i = k; i <= n; i++) {
+            printf("%d ", i);
+        }
+        for (i = 1; i < k; i++){
+        	printf("%d ", i);
+		}
     }
     return;
 }
