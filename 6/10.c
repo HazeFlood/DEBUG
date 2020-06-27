ade4dc5cf6ef62145f167945bffa3e5985369f6b
HazeFlood <67228421+HazeFlood@users.noreply.github.com>
2020-06-27 17:17:41 +0800

    Update 10.c

--- 6/10.c
+++ 6/10.c
@@ -1,65 +1,46 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-int main() {
-    char pms[3];
-    char change_pms[100][2];
-    int i;
-    i = 0;
-    int j;
-    int a;/* The original permission value of the file */ 
-    a = 0;
-    int b;
-    b = 0;
-    int c;
-    c = 0;
+int main() 
+{
+    char input[3];
+    int  initial = 1;
+    int  counter = 0;
+    int  i, j;
+    char operation[3];
     
-    /* Enter the current file permissions */ 
-    scanf("%s", pms);
- 
-    /* Enter file permissions to add or delete */ 
-    while (scanf("%s", change_pms[i++]) != EOF) {
-        scanf("%s", change_pms[i++]); 
+    scanf("%s", input);
+    if (input[0] == 'r' && input[1] == 'w' && input[2] == 'x') {
+        counter = 7;
+    } else if (input[0] == 'r' && input[1] == 'w') {
+        counter = 6;
+    } else if (input[0] == 'r' && input[1] == 'x') {
+        counter = 5;
+    } else if (input[0] == 'w' && input[1] == 'x') {
+        counter = 3;
+    } else if (input[0] == 'r') {
+        counter = 4;
+    } else if (input[0] == 'w') {
+        counter = 2; 
+    } else if (input[0] == 'x') {
+        counter = 1;
     }
-    /* Determines the permissions of the original file, expressed in decimal */ 
-    for (i = 0; i < strlen(pms); i++) {
-        if (pms[i] == 'r') {
-            a += 4;
-        } else if (pms[i] == 'w') {
-            a += 2;
+    while (scanf("%s", operation) != EOF) {
+        if (operation[0] == '+' && operation[1] == 'r') {
+            counter += 4;
+        } else if (operation[0] == '+' && operation[1] == 'w') {
+            counter += 2;
+        } else if (operation[0] == '+' && operation[1] == 'x') {
+            counter += 1;   
+        } else if (operation[0] == '-' && operation[1] == 'r') {
+            counter -= 4; 
+        } else if (operation[0] == '-' && operation[1] == 'w') {
+            counter -= 2;
         } else {
-            a += 1;
-        } 
-    }
-    
-    for (i = 0; i < strlen(change_pms[i]); i++) {
-        j = 0;
-        if (change_pms[i][j] == '+') {
-            if (change_pms[i][j + 1] == 'r') 
-           {
-               b += 4;   
-            } else if (change_pms[i][j + 1] == 'w') 
-           {
-                b += 2;
-            } else 
-           {
-                b += 1;
-            }
-        } else {
-            if (change_pms[i][j + 1] == 'r') 
-           {
-               b -= 4;   
-            } else if (change_pms[i][j + 1] == 'w') 
-           {
-                b -= 2;
-            } else 
-           {
-                b -= 1;
-            }
+            counter -= 1;
         }
     }
-    printf("%d", a + b);
-    
+    printf("%d", counter);
+      
     return 0;
 }
