9cae439e0538c8dcc3e5a2a68fdff5f63e5cc71c
root <2014164192@qq.com>
2020-06-28 09:17:55 +0800

    6,8

--- 13/6.c
+++ 13/6.c
@@ -18,22 +18,18 @@ int main() {
 
 Node *circle_create(int n) {
     Node *temp, *new_node, *head;
-    int i;
+    int i
 
-    // 创建第一个链表节点并加数据
     temp = (Node *) malloc(sizeof(Node));
     head = temp;
     head->data = 1;
 
-    // 创建第 2 到第 n 个链表节点并加数据
     for(i = 2; i <= n; i++) {
         new_node = (Node *) malloc(sizeof(Node));
         new_node->data = i;
         temp->next = new_node;
         temp = new_node;
     }
-
-    // 最后一个节点指向头部构成循环链表
     temp->next = head;
 
     return head;
@@ -48,7 +44,6 @@ void count_off(Node *head, int n, int k, int m) {
         temp = temp->next;
         counter++;
     } 
-    //printf("temp->data = %d\n", temp->data);
     if (m != 1) {
     for (j = 0; j < n; j++) {
         for (i = 0; i < m - 1; i++) {
