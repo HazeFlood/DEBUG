5212da6f809ddc6671c1072753aff6924b99e65d
root <2014164192@qq.com>
2020-06-28 08:41:00 +0800

    review and rewrite

--- 8/2.c
+++ 8/2.c
@@ -12,7 +12,7 @@ int main()
     char max[101];
     
     for (int i = 0; i < n; i++) {
-         gets(names[i]);
+         fgets(names[i]);
     }
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n - 1 - i; j++) {
--- 9/10.c
+++ 9/10.c
@@ -12,7 +12,9 @@ int f(int n)
 int main() 
 {
     int n;
+
     scanf("%d", &n);
     printf("%d", f(n));
+
     return 0;
 }
