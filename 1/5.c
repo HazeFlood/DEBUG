a531504f2fecd73af2840fe2d00db126c1629e24
shgal637 <67228666+shgal637@users.noreply.github.com>
2020-06-28 10:15:30 +0800

    Update 5.c

--- 1/5.c
+++ 1/5.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+
 #define EPSILON 1e-7
 
 double bisection(int p, int q, double (*func)(int, int, double));
@@ -19,17 +20,18 @@ double bisection(int p, int q, double (*func)(int, int, double))
     double a;
     double b;
     double c;
+	
     a = 20;
     b = -20;
     c = (a + b)/ 2;
     while(fabs(f(p,q,c)) >= EPSILON) {
         if((f(p,q,a) * f(p,q,c)) < 0) {
-			b = c;
-			c = (a + b)/2;
+	    b = c;
+	    c = (a + b)/2;
         }
         if((f(p,q,b) * f(p,q,c)) < 0) {
-			a = c;
-			c = (a + b)/2;
+	    a = c;
+	    c = (a + b)/2;
         }
     }
     return c;
