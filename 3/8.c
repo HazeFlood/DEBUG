afd4a87d85d009109359ab18beb797141cc440eb
HazeFlood <67228421+HazeFlood@users.noreply.github.com>
2020-06-29 22:54:51 +0800

    Update 8.c

--- 3/8.c
+++ 3/8.c
@@ -1,65 +1,62 @@
 #include <stdio.h>
 
-int main() 
+int main()
 {
     int matrix[100][100];
     int m, n;
-    int i,j;
-    int count = 0;
+    int i, j;
+    int count=0;
     int max;
     
-    /* input m & n & matrix */
-	scanf("%d %d", &m, &n);
-	
-	for (i = 0; i < m; i ++) {
+    /*input m & n & matrix*/
+	scanf("%d %d",&m, &n);
+	for (i = 0; i < m; i++) {
 		for (j = 0; j < n; j++) {
-			scanf("%d", &matrix[i][j]);
-			if (j < n-1) {
-			    scanf(" ");
-			}
+			scanf("%d",&matrix[i][j]);
 		}
 	}
+	max=m*n;
 	
-	max = m * n;
-	
-	/* output the first line */
-	for (j = 0; j < n; j ++) {
-		printf("%d ", matrix[0][j]);
-		count ++;
+	/*output the first line*/
+	for (j = 0; j < n; j++) {
+		printf("%d",matrix[0][j]);
+        if (j != n - 1 || m >1) {
+            printf(" ");
+        }
+		count++;
 	}
-	
-	do {	
-		int matrix_[n][m];
 		
-        /* rotate the matrix to -90 degree which becomes matrix_ */
-		for (i = 0; i < n; i ++) {
-			for (j = 0; j < m-1; j ++) {
+	do{	
+		int matrix_[n][m];/*rotate the matrix to -90 degree which becomes matrix_*/
+
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < m-1; j++) {
 				matrix_[i][j] = matrix[j+1][n-1-i];
+
 			}
+
 		}
 		
-		for (j = 0; j < m - 1; j ++) {
-			printf("%d", matrix_[0][j]);
-			count ++;
-			if (count < max) {
-			    printf(" ");
-			}
+		for(j = 0; j < m-1; j++) {
+			printf("%d",matrix_[0][j]);
+			count++;
+			if(count<max)
+				printf(" ");
 		}
 	
-		/* cover original matrix with new matrix_ */	
-		for (i = 0; i < n; i ++) {
-			for (j = 0; j < m; j ++) {
+		/*cover original matrix with new matrix_*/
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < m; j++) {
 				matrix[i][j] = matrix_[i][j];
 			}
 		}
 		
 		int t;
-		
 		t = m;
 		m = n;
-		n = t - 1;
-		/* m=n;n=m-1; no use t.This is so wrong!!! */
-	} while (m >= 1 && n >= 1);
+		n = t-1;
+			
+	}while (m >= 1 && n >= 1);
 	
     return 0;
 }
