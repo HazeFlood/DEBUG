1f6a23ed08e23a2bf55fd8e444ff829f840514dc
HazeFlood <67228421+HazeFlood@users.noreply.github.com>
2020-06-29 22:57:19 +0800

    Update 10.c

--- 3/10.c
+++ 3/10.c
@@ -1,74 +1,59 @@
-int main() 
-{
+#include <stdio.h>
+
+int main() {
     int matrix[100][100];
-    int m, n;
+    int m, n, i, j, num, count;
     int k = 0;
     int l = 0;
-    int i, j;
-    int num;
-    int cou = 0;
     
     scanf("%d %d", &m, &n);
-    
+    num = m * n;
+    count = 0;
     for (i = 0; i < m; i++) {
         for (j = 0; j < n; j++) {
             scanf("%d", &matrix[i][j]);
         }
     }
-      
-    while (k < m && l < n) {
-        /* Print the first row from the remaining rows */
-        for (i = l; i < n; i++)
-        {
-            printf("%d", matrix[k][i]);
-			cou++;
-			if (cou != m * n) {
-				printf(" ");
+
+   for (j = 0; j < n; j++) {
+		printf("%d",matrix[0][j]);
+        if (j != n - 1 || m >1) {
+            printf(" ");
+        }
+		count++;
+	}
+		
+	do{	
+		int matrix_[n][m];//rotate the matrix to -90 degree which becomes matrix_
+
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < m-1; j++) {
+				matrix_[i][j] = matrix[j+1][n-1-i];
+
 			}
-        }
-        k++;
- 
-        /* Print the last column from the remaining columns */
-        for (i = k; i < m; i++)
-        {
-            printf("%d", matrix[i][n - 1]);
-			cou++;
-            if (cou != m * n) {
-                printf(" ");
-            }
 
-        }
-        n--;
- 
-        /* Print the last row from the remaining rows */
-        if (k < m)
-        {
-            for (i = n - 1; i >= l; i--)
-            {
-                printf("%d", matrix[m - 1][i]);
-				cou++;
-	            if (cou != m * n) {
-		            printf(" ");
-			    }
-            }
-            m--;
-        }
- 
-        /* Print the first column from the remaining columns */
-        if (l < n)
-        {
-            for (i = m - 1; i >= k; i--)
-            {
-                printf("%d", matrix[i][l]);
-				cou++;
-	            if (cou != m * n) {
-		            printf(" ");
-			    }
-            }
-            l++;    
-        }   
-	printf(" ");     
-    }
-
+		}
+		
+		for(j = 0; j < m-1; j++) {
+			printf("%d",matrix_[0][j]);
+			count++;
+			if(count < num)
+				printf(" ");
+		}
+	
+		//cover original matrix with new matrix_	
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < m; j++) {
+				matrix[i][j] = matrix_[i][j];
+			}
+		}
+		
+		int t;
+		t = m;
+		m = n;
+		n = t-1;
+			
+	}while (m >= 1 && n >= 1);
+	
     return 0;
 }
