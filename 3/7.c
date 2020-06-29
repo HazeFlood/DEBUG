395f253bf410a2865b0001627dddc5a681702d31
HazeFlood <67228421+HazeFlood@users.noreply.github.com>
2020-06-29 22:52:33 +0800

    rewrite 7.c

--- 3/7.c
+++ 3/7.c
@@ -3,70 +3,88 @@
 int main() 
 {
     int matrix[100][100];
-    int m, n, i, j;
-    int startX;
-    int startY;
-    int endX;
-    int endY;
+    int m, n, i, j, count = 0;
+    int startX, startY, endX, endY;
    
     startX = 0;
     startY = 0;
-    endX = m;
-    endY = n;
-   
-    /* Enter the number of rows M and Columns N */
+
     scanf("%d %d", &m, &n);
     
-    /* Enter a Matrix of M * N */
+    endX  = m;
+    endY  = n;
+    
     for (i = 0; i < m; i++) {
         for (j = 0; j < n; j++) {
             scanf("%d", &matrix[i][j]);
         }
     }
    
-    /* Conditions for determining the number of cycles in a cycle */
     while (startX * 2 < m && startY * 2 < n) {
         endX = m - 1 - startX;
         endY = n - 1 - startY;
-        /* Output Step 1 */
-        if (startY < endY) {
-            for (j = startY; j <= endY; j++) {
-                printf("%d", matrix[startX][j]);
-                if (j <= endY) {
+        /*如果只有一列*/ 
+        if (startY == endY) {
+        	for (j = startX; j <= endX; j++) {
+        		    printf("%d", matrix[j][endY]);
+                    count++;
+                    if (j != endX) {
+                        printf(" ");
+                    }
+			    
+			}
+		break;
+		} else if (startX == endX) {
+        	for(j=startY; j <= endY; j++) {
+        		printf("%d", matrix[endX][j]);
+                count ++;
+	            if (j != endY){
                     printf(" ");
                 }
-            }
-        }
-          
-        /* The second step of output satisfies the following conditions */
-        if (startX < endX) {
-            for (j = startX + 1; j <= endX; j++) {
-                printf("%d", matrix[j][endY]);
-                if (j <= endY) {
-                    printf(" ");
-                }
-            }
-        }
-        /* The third step of output satisfies the following conditions */
-        if (endX > startX && startY < endY) {
-            for (j = endY - 1; j >= startY; j--) {
-                printf("%d", matrix[endX][j]);
-                if (j >= startY) {
-                    printf(" ");
-                }
-            }
-        }
-        /* The fourth step of output satisfies the following conditions */
-        if (endX - 1 > startX && startY < endY) {
-            for (i = endX - 1; i >= startX + 1; i--) {
-                printf("%d ", matrix[i][startX]);
-                if (i >= startX + 1) {
-                    printf(" ");
-                }
-            }
-        }
-        startX ++;
-        startY ++;
+			}
+			break;
+		} else {
+			if (startY < endY) {
+            	for (j = startY; j <= endY; j++) {
+                	printf("%d", matrix[startX][j]);
+                    count++;
+                	if (count != m*n) {
+                    	printf(" ");
+                	}
+            	}
+        	}
+        	if (startX < endX) {
+            	for (j = startX + 1; j <= endX; j++) {
+                	printf("%d", matrix[j][endY]);
+                	count++;
+                	if (count != m*n) {
+                    	printf(" ");
+                	}
+            	}
+        	}
+        	if (endX > startX && startY < endY) {
+            	for (j = endY - 1; j >= startY; j--) {
+                	printf("%d", matrix[endX][j]);
+                	count++;
+                	if (count != m*n) {
+                    	printf(" ");
+                	}
+            	}
+        	}
+        /*满足以下条件输出第四步*/
+        	if (endX - 1 > startX && startY < endY) {
+            	for (i = endX - 1; i >= startX + 1; i--) {
+                	printf("%d", matrix[i][startX]);
+                	count++;
+                	if (count != m*n) {
+                    	printf(" ");
+                	}
+            	}
+        	}
+        	
+		}
+		startX++;
+        startY++;    
     }
     
     return 0;
